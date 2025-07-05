cfa631bbdac91974a3c9fba8a0b5024529a965a3
Manas Bhutada <156465448+Manas-Bhutada@users.noreply.github.com>
2025-07-05 15:59:01 +0530

    Update medibot.py

--- medibot.py
+++ medibot.py
@@ -70,7 +70,7 @@ def main():
         Start the answer directly. No small talk.
         """
 
-        HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.1"
+        HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
         HF_TOKEN = os.environ.get("HF_TOKEN")
 
         try:
